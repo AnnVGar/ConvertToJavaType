7365cf2b7d8e6046c32adf5b5b4b0630b2266827
AnnVGar <garshina.av@gmail.com>
2022-10-03 11:29:46 +0300

    ConvertUToLong один класс для всех беззнаковых типов

--- src/impl/ConvertUToLong.java
+++ src/impl/ConvertUToLong.java
@@ -1,6 +1,7 @@
 package impl;
 
 import core.IConvertToLong;
+import exception.TooLargeNumberException;
 
 public class ConvertUToLong implements IConvertToLong {
 
@@ -8,12 +9,18 @@ public class ConvertUToLong implements IConvertToLong {
 	public Long convert(byte[] bytes) {
 		if (bytes != null) {
 			long result = 0;
-			for (int i = 0; i < bytes.length; i++) {
-				result = result | ((bytes[i] & 0xFFL) << (8 * i));
+			try {
+				if (bytes.length == 8 && (bytes[bytes.length - 1] >> 7 & 1) == 1) {
+
+					throw new TooLargeNumberException("Unable convert ulong to long");
+				}
+				for (int i = 0; i < bytes.length; i++) {
+					result = result | ((bytes[i] & 0xFFL) << (8 * i));
+				}
+			} catch (TooLargeNumberException e) {
+				e.printStackTrace();
 			}
-
 			return result;
-
 		}
 		return null;
 	}
--- src/main/ConvertToJavaType.java
+++ src/main/ConvertToJavaType.java
@@ -73,21 +73,21 @@ public class ConvertToJavaType {
 
 	public static ResultSet selectDataFromTable() throws SQLException {
 		Statement stmt = connection.createStatement();
-		return stmt.executeQuery("(SELECT DataType, DataValue FROM PropertyValue  )");
-//				+ "WHERE DataType = 15"// )");
-//				+") union (Select 0xD093 as DataValue)");             //test type = 4      Ã    
-//				+") union (Select 0x87  as DataValue)");             //test type = 5      -121      
-//	            +") union (Select 0xFF  as DataValue)");               //test type = 6      255
-//				+") union (Select 0xD08A  as DataValue)");             //test type = 7      -30000
-//		        +") union (Select 0xE8FD  as DataValue)");             //test type = 8      65000
-//	            +") union (Select 0x00317280  as DataValue)");         //test type = 9              -2 140 000 000
-//	            +") union (Select 0x8BDDFA7F  as DataValue)");         //test type = 9              2 147 147 147
-//          	+") union (Select 0x005ED0B2  as DataValue)");         //test type = 10      3 000 000 000
-//          	+") union (Select 0x00007C1DAF931983  as DataValue)"); //test type = 11         -9 000 000 000 000 000 000
+		return stmt.executeQuery("(SELECT DataType, DataValue FROM PropertyValue "// )");
+				+ "WHERE DataType = 10"// )");
+//				+") union (Select 4 as DataType, 0xD093 as DataValue)");             //test type = 4      Ã    
+//				+") union (Select 5 as DataType,0x87  as DataValue)");             //test type = 5      -121      
+//	            +") union (Select 6 as DataType, 0xFF  as DataValue)");               //test type = 6      255
+//				+") union (Select 7 as DataType, 0xD08A  as DataValue)");             //test type = 7      -30000
+//		        +") union (Select 8 as DataType, 0xE8FD  as DataValue)");             //test type = 8      65000
+//	            +") union (Select 9 as DataType, 0x00317280  as DataValue)");         //test type = 9              -2 140 000 000
+//	            +") union (Select 9 as DataType,0x8BDDFA7F  as DataValue)");         //test type = 9              2 147 147 147
+          	+") union (Select 10 as DataType,0x005ED0B2  as DataValue)");         //test type = 10      3 000 000 000
+//          	+") union (Select 11 as DataType,0x00007C1DAF931983  as DataValue)"); //test type = 11         -9 000 000 000 000 000 000
 //	            +") union (Select 12 as DataType,  0x0000E8890423C78A  as DataValue)"); //test type = 12      TooLargeNumberException
 //		        +") union (Select 12 as DataType,  0x0000E8890423C700  as DataValue)"); //test type = 12      56052022765944832
 
-//          	+") union (Select 0xE78745C1  as DataValue)");         //test type = 13      -12,34568
+//          	+") union (Select 13 as DataType,0xE78745C1  as DataValue)");         //test type = 13      -12,34568
 //	            +") union (Select 14 as DataType,  0x012F83B4E6C75EC0  as DataValue)"); //test type = 14      -123.123456123456
 //				+ ") union (Select 15 as DataType,  0x61C80CCECB5C00000000000000000180 as DataValue)"); // test type = 15      -10203040506070,5
 //		+ ") union (Select 15 as DataType,  0x01000000000000000000000000000580  as DataValue)"); // test type = 15      -0.00001
